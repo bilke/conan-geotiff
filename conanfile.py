b9954ab84c8f6bc66ac0002c51d7cd8378fec6e1
Lars Bilke <lars.bilke@ufz.de>
2016-12-15 10:24:21 +0100

    Disabled function check and added ZLIB option as TIFF is built with it.

--- conanfile.py
+++ conanfile.py
@@ -41,13 +41,41 @@ class LibgeotiffConan(ConanFile):
 +	 ARCHIVE DESTINATION lib )
 '''
         patch(patch_string=patch_content, base_path=self.ZIP_FOLDER_NAME)
+
+        patch_content2 = '''--- CMakeLists.txt	2016-12-15 10:01:04.000000000 +0100
++++ CMakeLists.txt	2016-12-15 10:08:40.000000000 +0100
+@@ -189,22 +189,6 @@
+     endif ()
+
+     IF(TIFF_FOUND)
+-        # Confirm required API is available
+-        INCLUDE(CheckFunctionExists)
+-        SET(CMAKE_REQUIRED_LIBRARIES ${TIFF_LIBRARIES})
+-
+-        CHECK_FUNCTION_EXISTS(TIFFOpen HAVE_TIFFOPEN)
+-        IF(NOT HAVE_TIFFOPEN)
+-            SET(TIFF_FOUND) # ReSET to NOT found for TIFF library
+-            MESSAGE(FATAL_ERROR "Failed to link with libtiff - TIFFOpen function not found")
+-        ENDIF()
+-
+-        CHECK_FUNCTION_EXISTS(TIFFMergeFieldInfo HAVE_TIFFMERGEFIELDINFO)
+-        IF(NOT HAVE_TIFFMERGEFIELDINFO)
+-            SET(TIFF_FOUND) # ReSET to NOT found for TIFF library
+-            MESSAGE(FATAL_ERROR "Failed to link with libtiff - TIFFMergeFieldInfo function not found. libtiff 3.6.0 Beta or later required. Please upgrade or use an older version of libgeotiff")
+-        ENDIF()
+-
+         INCLUDE_DIRECTORIES(${TIFF_INCLUDE_DIR})
+         ADD_DEFINITIONS(-DHAVE_TIFF=1)
+     ENDIF(TIFF_FOUND)
+'''
+        patch(patch_string=patch_content2, base_path=self.ZIP_FOLDER_NAME)
         cmake = CMake(self.settings)
         if self.settings.os == "Windows":
             self.run("IF not exist _build mkdir _build")
         else:
             self.run("mkdir _build")
         cd_build = "cd _build"
-        CMAKE_OPTIONALS = ""
+        CMAKE_OPTIONALS = "-DWITH_ZLIB=ON "
         if self.options.shared == False:
             CMAKE_OPTIONALS += "-DBUILD_SHARED_LIBS=OFF"
         else:
