2843848ae762f8ea5c8b6beb8afd7909b1e7ddc0
Lars Bilke <lars.bilke@ufz.de>
2017-07-20 22:13:40 +0200

    Updated to conan 0.25 and new travis layout.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,12 @@
 import os
 from conans import ConanFile, CMake
-from conans.tools import download, unzip, patch, replace_in_file
+from conans.tools import download, unzip
 
 class LibgeotiffConan(ConanFile):
     name = "libgeotiff"
+    description = """Libgeotiff is a library for reading, and writing GeoTIFF
+                     information tags."""
+
     version = "1.4.2"
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
@@ -14,8 +17,6 @@ class LibgeotiffConan(ConanFile):
     url="http://github.com/bilke/conan-geotiff"
     license="http://trac.osgeo.org/geotiff/"
 
-    INSTALL_DIR = "_install"
-
     def source(self):
         zip_name = "%s.zip" % self.version
         download("https://github.com/ufz/geotiff/archive/%s" % zip_name, zip_name)
@@ -23,26 +24,16 @@ class LibgeotiffConan(ConanFile):
         os.unlink(zip_name)
 
     def build(self):
-        cmake = CMake(self.settings)
-        if self.settings.os == "Windows":
-            self.run("IF not exist _build mkdir _build")
-        else:
-            self.run("mkdir _build")
-        cd_build = "cd _build"
-        CMAKE_OPTIONALS = "-DWITH_ZLIB=ON "
+        cmake = CMake(self)
+        cmake.definitions["WITH_ZLIB"] = "ON"
         if self.settings.os == "Linux":
-            CMAKE_OPTIONALS += "-DCMAKE_POSITION_INDEPENDENT_CODE=ON "
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
         if self.options.shared == True:
-            CMAKE_OPTIONALS += "-DBUILD_SHARED_LIBS=ON "
+            cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
         if self.options.utilities == False:
-            CMAKE_OPTIONALS += "-DWITH_UTILITIES=OFF "
-        self.run("%s && cmake .. -DCMAKE_INSTALL_PREFIX=../%s %s %s" % (cd_build, self.INSTALL_DIR, cmake.command_line, CMAKE_OPTIONALS))
-        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
-        self.run("%s && cmake --build . --target install %s" % (cd_build, cmake.build_config))
-
-    def package(self):
-        self.copy("FindLibGeoTiff.cmake", ".", ".")
-        self.copy("*", dst=".", src=self.INSTALL_DIR)
+            cmake.definitions["WITH_UTILITIES"] = "OFF"
+        cmake.configure(build_dir="build")
+        cmake.build(target="install")
 
     def package_info(self):
         if self.settings.os == "Windows" and self.settings.build_type == "Debug":
